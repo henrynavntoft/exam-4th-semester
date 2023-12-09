f5cf20a6fe2aada021014be666aa022ddf44dac7
Henry Navntoft <henrylnavntoft@gmail.com>
2023-12-09 15:21:13 +0100

    disable api route

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,24 +1,24 @@
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
-);
+// const supabase = createClient(
+//   process.env.SUPABASE_URL,
+//   process.env.SUPABASE_SERVICE_KEY
+// );
 
-export default async function route(req, res) {
-  if (req.method === "POST") {
-    const { data, error } = await supabase.from("Footer").insert([req.body]);
+// export default async function route(req, res) {
+//   if (req.method === "POST") {
+//     const { data, error } = await supabase.from("Footer").insert([req.body]);
 
-    if (error) {
-      // Send error response
-      return res.status(500).json({ error: error.message });
-    }
+//     if (error) {
+//       // Send error response
+//       return res.status(500).json({ error: error.message });
+//     }
 
-    // Send success response
-    return res.status(200).json(data);
-  } else {
-    // Handle non-POST requests
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
-  }
-}
+//     // Send success response
+//     return res.status(200).json(data);
+//   } else {
+//     // Handle non-POST requests
+//     res.setHeader("Allow", "POST");
+//     res.status(405).end("Method Not Allowed");
+//   }
+// }
